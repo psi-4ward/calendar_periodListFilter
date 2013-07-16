10d8245fa6a2d92156de7b1ae681158c9b5d444e
Christoph Wiechert <wio@psitrax.de>
2013-07-16 11:16:45 +0200

    added some languages: thanks to Andreas Steinkellner!

---
+++ languages/de/tl_module.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @copyright 4ward.media 2013 <http://www.4wardmedia.de>
+ * @author Christoph Wiechert <wio@psitrax.de>
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['eventlist_start'] = array('FE Start-Datum Standardwert', 'Setzen des Standardwertes für dem Start-Datum Filter. (0 für aktuelles Datum; +/-30 für 30 Tage in der Zukunft/Vergangenheit; ein Datum YYYY-mm-dd für ein fixes Datum)');
+$GLOBALS['TL_LANG']['tl_module']['eventlist_stop'] = array('FE End-Datum Standardwert', 'Setzen des Standartwertes für den End-Datum Filter. (0 für aktuelles Datum; +/-30 für 30 Tage in der Zukunft/Vergangenheit; ein Datum YYYY-mm-dd für ein fixes Datum)');
